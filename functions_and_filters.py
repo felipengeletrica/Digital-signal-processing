ec6ad02aee71abd8d0caa0ff77f0ee21dbc82d11
felipevargas <felipeng.eletrica@gmail.com>
2019-11-25 22:04:59 -0300

    #ADD: PowerPointer

--- functions_and_filters.py
+++ functions_and_filters.py
@@ -143,12 +143,45 @@ class functions_and_filters(object):
         :param audio_samples_rate:
         :return:
         """
+
         nyq = (audio_samples_rate / 2)
+        fs = nyq # Sample frequency (Hz)
         low = 450 / nyq
+        f0 = 500
         high = 550 / nyq
         order = 3
-        i, u = signal.butter(order, [low, high], btype='bandstop')
-        y = lfilter(i, u, audio_samples)
+        b, a = signal.butter(order, [low, high], btype='bandstop')
+
+        print(f'NUM:{a}')
+        print(f'DEN:{b}')
+        y = lfilter(b, a, audio_samples)
+
+        # Frequency response
+        freq, h = signal.freqz(b, a, fs=fs)
+        # Plot
+        fig, ax = plt.subplots(2, 1, figsize=(8, 6))
+
+        # x coordinates for the lines
+        xcoords = [f0]
+        # colors for the lines
+        colors = ['r']
+        for xc, c in zip(xcoords, colors):
+            ax[0].axvline(x=xc, label='line at x = {}'.format(xc), c=c)
+
+        ax[0].plot(freq, 20 * np.log10(abs(h)), color='blue')
+        ax[0].set_title("Frequency Response")
+        ax[0].set_ylabel("Amplitude (dB)", color='blue')
+        ax[0].set_xlim([0, fs / 2])
+        ax[0].set_ylim([-40, 40])
+        ax[0].grid()
+        ax[1].plot(freq, np.unwrap(np.angle(h)) * 180 / np.pi, color='green')
+        ax[1].set_ylabel("Angle (degrees)", color='green')
+        ax[1].set_xlabel("Frequency (Hz)")
+        ax[1].set_xlim([0, fs / 2])
+        ax[1].set_yticks([-90, -60, -30, 0, 30, 60, 90])
+        ax[1].set_ylim([-180, 180])
+        ax[1].grid()
+        plt.show()
         return y
 
     def lowPassFilter(self, audio_samples, audio_samples_rate):
